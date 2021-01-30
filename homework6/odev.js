01724a75b2660cc6857c7940c67d1f46b3d4bc0a
brcthn <burcu1818@gmail.com>
2021-01-30 14:04:43 +0100

    Added bootstrap-modal for detail button

--- homework6/odev.js
+++ homework6/odev.js
@@ -8,10 +8,32 @@ window.removePet = (id) => {
     console.log(id) // remove the pet with given id
 };
 
+window.generateDetailModal = (pet) => {
+    return ` <div class="modal fade" id="exampleModal${pet.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">${pet.name} </h5>
+        </div>
+        <div class="modal-body">
+        
+          <p>${pet.description}</p>
+        </div>
+        <div class = "modal-footer>
+          <button type="button" class ="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>`
+
+}
+
 window.openPetDetail = (id) => {
     fetch(`${window.mockApiUrl}${id}`,{
         method: "GET",
-        }).then((response) => response.json()).then((data)=>
-        console.log(data))
+        }).then((response) => response.json()).then((data)=>{
+            const petModalHTMl =  generateDetailModal(data);
+            document.querySelector("body").innerHTML += petModalHTMl;
+            $(`#exampleModal${id}`).modal('show');
+    })
         .catch((err)=>console.log(err.message))
 };
